123fad565a17bcc8ac249302622777ddd14d6f5a
Tim McGlothin <tmcglothin@llbean.com>
2023-04-27 11:21:02 -0400

    relay_control initial commit

---
+++ relay_control.py
@@ -0,0 +1,34 @@
+import RPi.GPIO as GPIO
+
+# List of GPIO outputs
+gpio_outputs = [4, 27, 22, 5, 6, 26, 23, 24]
+
+# Set up the GPIO mode and warnings
+GPIO.setmode(GPIO.BCM)
+GPIO.setwarnings(False)
+
+# Set up the GPIO pins as outputs and set them to HIGH (relay off)
+for output in gpio_outputs:
+    GPIO.setup(output, GPIO.OUT)
+    GPIO.output(output, GPIO.HIGH)
+
+# Function to control the relay
+
+
+def control_relay(gpio_output, state):
+    if state.lower() == "on":
+        GPIO.output(gpio_output, GPIO.LOW)  # Activate the relay
+    elif state.lower() == "off":
+        GPIO.output(gpio_output, GPIO.HIGH)  # Deactivate the relay
+    else:
+        print(f"Invalid state '{state}'. Use 'on' or 'off'.")
+
+
+# Example usage of the function
+try:
+    control_relay(4, "on")
+    control_relay(27, "off")
+except KeyboardInterrupt:
+    # Clean up when the program is terminated by Ctrl+C
+    print("\nCleaning up and exiting...")
+    GPIO.cleanup()
